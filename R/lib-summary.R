880088d9cf886f334f5434b4396654ad2413c5ff
Claire Hornby <claire.hornby@gmail.com>
2024-11-04 12:54:40 -0600

    added description

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -1,8 +1,7 @@
 lib_summary<- function () {
-  pkgs<- utiles::installed.packages()
+  pkgs<- utils::installed.packages()
   pkg_tbl<-table(pkgs[,"LibPath"])
   pkg_df<-as.data.frame(pkg_tbl, stingsAsFactors=FALSE)
   names(pkg_df) <- c("Library", "n_packages")
   pkg_df
 }
-
